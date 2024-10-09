7fe4c59698806a5a1a05e0fd73eb22ec06b1c77e
Eduardo Lozano <eduardo.azevedo@waydatasolution.com>
2024-10-09 13:04:30 -0300

    adicionando servico do condominio e realizando insercao, e obtencoes por filtros

--- WebAngular/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
-
-export const routes: Routes = [
-    {path: '', redirectTo: 'register', pathMatch: 'full'},
-    {path: 'home', title: 'Home'},
-    {path: 'register', title: 'Registrar', component: RegisterComponent}
-];
