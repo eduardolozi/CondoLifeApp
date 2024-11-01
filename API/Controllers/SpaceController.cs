7c4ac538bbe6560f214a3ff5be4d5545d319514e
Eduardo Lozano <eduardo.azevedo@waydatasolution.com>
2024-11-01 14:49:57 -0300

    criacao da api dos espacos

---
+++ API/Controllers/SpaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class SpaceController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
