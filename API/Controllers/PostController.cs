b7cfeb0bd0984b57b2cb247d2acf55d71ccd0d1a
Eduardo Lozano <eduardo.azevedo@waydatasolution.com>
2024-11-23 15:06:13 -0300

    criacao das tabelas do contexto de postagens

---
+++ API/Controllers/PostController.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+public class PostController : ControllerBase
+{
+    // public IActionResult Create(Post post)
+    // {
+    //     
+    // }
+}
---
+++ Domain/Models/PostMedias.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Models;
+
+public class PostMedias
+{
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public int PostId { get; set; }
+    [JsonIgnore]
+    public Post Post { get; set; }
+}
